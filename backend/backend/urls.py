8018ab677d8edefe885f43f9e9a3f3f54dd6c7ed
rakinul-islam-raju <rakibul.islam7772588@gmail.com>
2022-05-17 16:25:53 +0600

    [ADD] Added refresh token API

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,9 +4,16 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.static import serve
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("core.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/shop/", include("shop.urls")),
     path("api/orders/", include("orders.urls")),
     # static
